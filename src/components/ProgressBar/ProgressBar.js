cc3a2e2f03fef5c1642d888474f2dc77aefc0f12
OkeyIke <benjaminwerner16@gmail.com>
2023-10-06 23:55:43 +0200

    Add custom build ProgressBar component as compatible alternative

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -12,7 +12,6 @@ const SIZES = {
     '--radius': 4 + 'px',
   },
   medium: {
-
     '--height': 12 + 'px',
     '--padding': 0,
     '--radius': 8 + 'px',
@@ -24,45 +23,87 @@ const SIZES = {
   },
 }
 
+/* Self Build Progress Bar */
+
 const ProgressBar = ({ value, size }) => {
   const styles = SIZES[size]
   return (
-    <>
-      <StyledBar max='100' value={value} style={styles}></StyledBar>
+    <Wrapper
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemin='0'
+      aria-valuemax='100'
+      style={styles}
+    >
+      <BarWrapper>
+        <Bar value={value} />
+      </BarWrapper>
       <VisuallyHidden>{value}%</VisuallyHidden>
-    </>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(props) => props.value + '%'};
+  height: var(--height);
+  border-radius: 4px 0 0 4px;
+`
+
+/* Semantic way using the <progress> element. */
+/* Currently not working with all web browser properly */
+
+/* const ProgressBar = ({ value, size }) => { */
+/*   const styles = SIZES[size] */
+/*   return ( */
+/*     <> */
+/*       <StyledBar max='100' value={value} style={styles}></StyledBar> */
+/*       <VisuallyHidden>{value}%</VisuallyHidden> */
+/*     </> */
+/*   ) */
+/* } */
+
 /* Styles for ProgressBar */
 
-const StyledBar = styled.progress`
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border: none;
-  border-radius: var(--radius);
-  background-color: ${COLORS.transparentGray15};
-  height: var(--height);
-  width: 370px;
-  padding: var(--padding);
-  appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  &::-webkit-progress-bar {
-    background-color: ${COLORS.transparentGray15};
-  }
-
-  &::-moz-progress-bar {
-    background-color: ${COLORS.primary};
-  }
-
-  &[value]::-moz-progress-bar {
-    background-color: ${COLORS.primary};
-    border-radius: var(--radius);
-  }
-
-  &[value]::-webkit-progress-value {
-    background-color: ${COLORS.primary};
-    border-radius: var(--radius);
-  }
-`
+/* const StyledBar = styled.progress` */
+/*   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35}; */
+/*   border: none; */
+/*   border-radius: var(--radius); */
+/*   background-color: ${COLORS.transparentGray15}; */
+/*   height: var(--height); */
+/*   width: 370px; */
+/*   padding: var(--padding); */
+/*   appearance: none; */
+/*   -webkit-appearance: none; */
+/*   -moz-appearance: none; */
+/*   &::-webkit-progress-bar { */
+/*     background-color: ${COLORS.transparentGray15}; */
+/*   } */
+/**/
+/*   &::-moz-progress-bar { */
+/*     background-color: ${COLORS.primary}; */
+/*   } */
+/**/
+/*   &[value]::-moz-progress-bar { */
+/*     background-color: ${COLORS.primary}; */
+/*     border-radius: var(--radius); */
+/*   } */
+/**/
+/*   &[value]::-webkit-progress-value { */
+/*     background-color: ${COLORS.primary}; */
+/*     border-radius: var(--radius); */
+/*   } */
+/* ` */
 export default ProgressBar
