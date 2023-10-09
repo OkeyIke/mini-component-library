ffe9f6f7bf170007790cba5d2f84284cb5124c7c
OkeyIke <benjaminwerner16@gmail.com>
2023-10-09 07:37:07 +0200

    Add Select component with custom styling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from '../../constants'
+import Icon from '../Icon'
+import { getDisplayedValue } from './Select.helpers'
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const Select = ({ value, onChange, children }) => {
+  const displayedValue = getDisplayedValue(value, children)
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <CustomSelect>
+        {displayedValue}
+        <StyledIcon style={{ '--size': 24 + 'px' }}>
+          <Icon id='chevron-down' size={24} strokeWidth={2} />
+        </StyledIcon>
+      </CustomSelect>
+    </Wrapper>
+  )
+}
 
-export default Select;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const StyledSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+`
+
+const CustomSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: ${16 / 16}rem;
+  border: none;
+  border-radius: 8px;
+  ${StyledSelect}:focus + & {
+    outline: 2px dotted #212121; /* older Browser */
+    outline: auto 3px activeborder; /* Chrome */
+    outline: auto 3px -moz-mac-focusring; /* Firefox */
+    outline: 5px auto -webkit-focus-ring-color; /* Webkit, Safari */
+  }
+  ${StyledSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const StyledIcon = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+  pointer-events: none;
+`
+
+export default Select
